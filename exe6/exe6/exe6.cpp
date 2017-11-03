10bd0c22a4752b234aa2e375d10d9a0e202d9416
matheusgatao <mhevolve@gmail.com>
2017-11-03 21:12:35 -0200

    .

--- exe6/exe6/exe6.cpp
+++ exe6/exe6/exe6.cpp
@@ -2,10 +2,20 @@
 //
 
 #include "stdafx.h"
+#include <Windows.h>
 
+int numero1, numero2;
 
 int main()
 {
+	printf("Digite o primeiro numero:\n");
+		scanf_s("%i", &numero1);
+		printf("Digite o segundo numero:\n");
+		scanf_s("%i", &numero2);
+		numero1 = numero1 * numero2;
+		printf("O resultado da multiplicacao e %i\n\n", numero1);
+		system("pause");
+
     return 0;
 }
 
---
+++ exe7/exe7/exe7.cpp
@@ -0,0 +1,13 @@
+// exe7.cpp: Define o ponto de entrada para a aplicação de console.
+//
+
+#include "stdafx.h"
+#include <windows.h>
+
+
+int main()
+{
+	printf("");
+    return 0;
+}
+
