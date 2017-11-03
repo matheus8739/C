c3db091bc0765ae436dc529f2d52211db048f0ab
matheusgatao <mhevolve@gmail.com>
2017-11-03 21:22:26 -0200

    .

--- exe7/exe7/exe7.cpp
+++ exe7/exe7/exe7.cpp
@@ -2,12 +2,25 @@
 //
 
 #include "stdafx.h"
-#include <windows.h>
+#include <Windows.h>
 
+float n1, n2, n3, r1, r2, r3;
 
 int main()
 {
-	printf("");
-    return 0;
+	printf("Digitei o primeiro valor:\n");
+	scanf_s("%f", &n1);
+	printf("Digitei o segundo valor:\n");
+	scanf_s("%f", &n2);
+	printf("Digitei o terceiro valor:\n");
+	scanf_s("%f", &n3);
+	r1 = n1 + n2;
+	r2 = n2 + n3;
+	r3 = r1 * r2;
+	printf("O resultado e %f\n", r3);
+	system("pause");
+
+
+	return 0;
 }
 
